a99408754fc3a74a8283e59661154690d8d586bc
Davina Crahet <davina.crahet@gmail.com>
2024-04-30 01:43:24 +0200

    Modif icon home

--- front/src/components/Feature.js
+++ front/src/components/Feature.js
@@ -1,11 +1,23 @@
-const Feature = ({ title, alt, img, paragraph }) => {
+import { PropTypes } from "prop-types";
+import iconImage0 from "../assets/img/icon-0.png";
+import iconImage1 from "../assets/img/icon-1.png";
+import iconImage2 from "../assets/img/icon-2.png";
+
+const Feature = ({ title, alt, image, paragraph }) => {
   return (
     <div className="feature-item">
-      <img src={img} alt={alt} className="feature-item-icon" />
+      <img src={image} alt={alt} className="feature-item-icon" />
       <h3 className="feature-item-title">{title}</h3>
       <p>{paragraph}</p>
     </div>
   );
+};
+
+Feature.propTypes = {
+  paragraph: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
 };
 
 export default Feature;
--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,22 +1,22 @@
-import React from "react";
 import Banner from "../components/Banner";
 import Feature from "../components/Feature";
 import featuresData from "../data/features.json";
 
 const Home = () => {
-  const features = featuresData.features;
+  const featuresArray = featuresData.features;
+  console.log(featuresArray[0].image);
 
   return (
     <div className="main">
       <Banner />
       <section className="features">
         <h2 className="sr-only">Features</h2>
-        {features.map((feature, index) => (
+        {featuresArray.map((feature, index) => (
           <Feature
             key={index}
             title={feature.title}
             alt={feature.alt}
-            img={feature.image}
+            image={feature.image}
             paragraph={feature.paragraph}
           />
         ))}
