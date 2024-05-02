e8b3e58a0e454c15ec6c3c803713ae5a9818e61e
Davina Crahet <davina.crahet@gmail.com>
2024-05-02 14:03:25 +0200

    Ajout provider & store redux

--- front/src/index.js
+++ front/src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/index.scss";
 
+import { Provider } from "react-redux";
+import { mainStore } from "./redux";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={mainStore}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
